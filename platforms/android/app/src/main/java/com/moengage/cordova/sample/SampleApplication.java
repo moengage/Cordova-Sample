75ab4b71b69e2abf7b87dbc147365dedc2880ece
Sonal Rathi <sonal.rathi@moengage.com>
2018-09-10 18:24:04 +0530

    Remove app id and sender id

--- platforms/android/app/src/main/java/com/moengage/cordova/sample/SampleApplication.java
+++ platforms/android/app/src/main/java/com/moengage/cordova/sample/SampleApplication.java
@@ -10,14 +10,12 @@ import com.moengage.core.MoEngage.DATA_REGION;
 public class SampleApplication extends Application{
   public void onCreate(){
     super.onCreate();
-    MoEngage moEngage = new MoEngage.Builder(this, "DAO6UGZ73D9RTK8B5W96TPYN")
+    MoEngage moEngage = new MoEngage.Builder(this, "xxxxxxxxxxxxx")
         .setLogLevel(Logger.VERBOSE)
         .enableLocationServices()
         .setNotificationSmallIcon(R.drawable.icon)
         .setNotificationLargeIcon(R.drawable.icon)
-        .setSenderId("615448685370")
-        .setNotificationType(R.integer.notification_type_multiple)
-        .redirectDataToRegion(DATA_REGION.REGION_DEFAULT)
+        .setSenderId("xxxxxxxxxxxx")
         .build();
         MoEngage.initialise(moEngage);
   }
